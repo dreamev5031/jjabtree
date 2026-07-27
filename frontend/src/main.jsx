923df6b026ea76ddf0f5917f9225e8156abec4d5
dreamev5031 <bbhlin3@gmail.com>
2026-07-27 23:38:43 +0900

    Register privacy policy route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AdminPage from './pages/AdminPage'
+import PrivacyPage from './pages/PrivacyPage'
 import PublicPage from './pages/PublicPage'
 import './styles.css'
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<PublicPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
