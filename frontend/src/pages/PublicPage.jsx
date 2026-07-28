ac598a705b8b4fcb97ccf01160938c3702ea592e
dreamev5031 <bbhlin3@gmail.com>
2026-07-28 13:01:47 +0900

    Load Todaypicks hero banner styles

--- frontend/src/pages/PublicPage.jsx
+++ frontend/src/pages/PublicPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { apiRequest } from '../api'
 import Loading from '../components/Loading'
+import '../public-banner.css'
 
 const PAGE_SIZE = 10
 
